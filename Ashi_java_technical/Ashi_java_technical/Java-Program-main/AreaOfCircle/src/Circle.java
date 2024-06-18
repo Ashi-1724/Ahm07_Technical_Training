1a6315fc90e39d050867ebfd60f4760d192733f4
Ashi-1724 <150056513+Ashi-1724@users.noreply.github.com>
2024-06-18 11:47:39 +0530

    Delete Ashi_java_technical/Ashi_java_technical/Java-Program-main directory

--- Ashi_java_technical/Ashi_java_technical/Java-Program-main/AreaOfCircle/src/Circle.java
+++
@@ -1,9 +0,0 @@
-public class Circle {
-    // Declare a final variable for PI
-    public static final double PI = 3.14159;
-
-    // Method to calculate the area of the circle
-    public double calculateArea(double radius) {
-        return PI * radius * radius;
-    }
-}
--- Ashi_java_technical/Ashi_java_technical/Java-Program-main/Basic/src/Emp_Details.java
+++
@@ -1,9 +0,0 @@
-
-public class Emp_Details {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
